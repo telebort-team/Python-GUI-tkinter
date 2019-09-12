26a05b31519fde451b8e94daa98e9fc41751cf66
OoiBP <bpooi0702@gmail.com>
2019-09-12 15:24:02 +0800

    Add begin.py

--- Test/begin.py
+++ Test/begin.py
@@ -1,4 +1,12 @@
 import tkinter
 
-top = tkinter.Tk()
-top.mainloop()
+window = tkinter.Tk()
+
+# Rename the title of the window
+window.title("GUI")
+
+# Pack si used to show the object in the window
+label = tkinter.Label(window, text="Hello World!")
+label.pack()
+
+window.mainloop()
